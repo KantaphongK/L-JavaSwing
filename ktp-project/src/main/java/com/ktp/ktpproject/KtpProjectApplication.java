4614132edf4d2cb8d499ead4114dc9106ea23b28
KengN <k.kantaphong@kkumail.com>
2023-12-29 17:19:42 +0700

    feat:init cm

---
+++ ktp-project/src/main/java/com/ktp/ktpproject/KtpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ktp.ktpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtpProjectApplication.class, args);
+	}
+
+}
---
+++ ktp-project/src/test/java/com/ktp/ktpproject/KtpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktp.ktpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
